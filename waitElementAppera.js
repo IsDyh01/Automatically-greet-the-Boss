4978fc334cea133433bada92ae2871fcef2548ac
doki- <1335902682@qq.com>
2025-04-25 01:28:59 +0800

    feat: 7777

--- waitElementAppera.js
+++ waitElementAppera.js
@@ -1,13 +1,19 @@
 /**
  * 等待特定的元素出现
  */
-const waitElementAppera = async (PositioningStrategy, webDriver, until, eleName) => {
+const waitElementAppera = async (
+  PositioningStrategy,
+  webDriver,
+  until,
+  eleName
+) => {
   try {
     await webDriver.wait(until.elementLocated(PositioningStrategy), 60000);
   } catch (error) {
     console.log(error);
-    
-    return Promise.reject(`等待${eleName}元素已超时`)
+
+    return Promise.reject(`等待${eleName}元素已超时`);
   }
 };
 module.exports = waitElementAppera;
+console.log(1112222);
