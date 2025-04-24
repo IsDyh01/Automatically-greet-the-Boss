375bd742dbb8ac21c7b944275de20f1a4e3683ac
doki- <1335902682@qq.com>
2025-04-25 01:22:03 +0800

    feat: 2222

--- promptUtil.js
+++ promptUtil.js
@@ -1,18 +1,22 @@
-const inquire = require('inquirer');
+const inquire = require("inquirer");
 
 const prompt = inquire.createPromptModule();
 
 // prompt工具函数
 const inputPrompt = async (params) => {
-    const { message, name } = params
-    const answer = await prompt([{
-        type: 'input',
-        message,
-        name
-    }])
-    return answer;
-}
+  const { message, name } = params;
+  const answer = await prompt([
+    {
+      type: "input",
+      message,
+      name,
+    },
+  ]);
+  return answer;
+};
 
 module.exports = {
-    inputPrompt
-}
+  inputPrompt,
+};
+
+console.log(1111111);
